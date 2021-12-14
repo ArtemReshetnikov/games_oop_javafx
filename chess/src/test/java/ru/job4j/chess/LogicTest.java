830d0f0971a5b6c5b82b7f90ff70132acd4dd038
ArtemReshetnikov <majehed89@gmail.com>
2021-12-14 20:48:02 +0500

    2. Сокобан. Тестовое задание. [#75283]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -15,4 +15,29 @@ public class LogicTest {
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.H6);
     }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void moveWhenImpossible()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.C6);
+    }
+
+    @Test(expected = OccupiedCellException.class)
+    public void moveWhenCellOccupied()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.add(new BishopBlack(Cell.D2));
+        logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test(expected = FigureNotFoundException.class)
+    public void moveWhenFigureNotFound()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C3, Cell.H8);
+    }
 }
